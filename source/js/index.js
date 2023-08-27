9dafb8a4cb3e14da0224c747dbfe62548023c54b
Ivan <ivanfilippovw@gmail.com>
2023-08-27 11:39:01 +0400

    feat: nojs for interactive map

--- source/js/index.js
+++ source/js/index.js
@@ -1,5 +1,11 @@
 document.querySelector('body').classList.remove('page__body--nojs');
 
+document.querySelector('.contacts__map-wrapper').classList.remove('.contacts__map-wrapper--nojs');
+// логика, что если нет класса .contacts__map-wrapper--nojs, то статичная карта скрывается и отображается интерактивная
+
+document.querySelector('.more__button-link').classList.remove('.more__button-link--nojs');
+// логика, что если нет класса .more__button-link--nojs , то нужно удалить href
+
 document.addEventListener("DOMContentLoaded", function() {
   document.getElementById("hamburger__button").addEventListener("click", function() {
     document.querySelector("body").classList.toggle("open")
